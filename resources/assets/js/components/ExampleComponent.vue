c7f432e168a2cf938487e48f711822a0dc30645e
Milroy Fraser <milroy@outlook.com>
2018-02-10 02:48:28 +0530

    cleanup laravel

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,15 +1,9 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Example Component</div>
+    <div class="card card-default">
+        <div class="card-header">Example Component</div>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
+        <div class="card-body">
+            I'm an example component.
         </div>
     </div>
 </template>
